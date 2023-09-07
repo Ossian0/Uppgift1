1ea261bc17fe8feb9d2264e5f0ba91415b404eb6
Pingvu <nhlpool27@gmail.com>
2023-09-07 12:55:04 +0200

    Add project files.

---
+++ Uppgift1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System; // den är typ en bok (coder för progremering
+namespace Uppgift_1._1 // namn på upgiften
+{ // den skaffar en block för programer
+    class Program // vilken class är det
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur mår du"); // den skriver ut text
+            Console.WriteLine("Jag mår bra"); // den skriver ut text
+        }// den stänger den blocken som vi öppnade för progremering
+    }
+
+}
+      
+
